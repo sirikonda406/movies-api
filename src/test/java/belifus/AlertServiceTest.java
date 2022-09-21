aefcfac42fa96d83dca294beee48956626797e41
sirikonda406 <mahesh.sirikonda@gmail.com>
2022-09-21 21:55:42 +0200

    springboot changes

---
+++ src/test/java/belifus/AlertServiceTest.java
@@ -0,0 +1,11 @@
+package belifus;
+
+public class AlertServiceTest {
+
+    public static void main(String[] args) {
+
+        AlertService alertService=new AlertService(new AlertService.MapAlertDAO());
+
+        System.out.println(alertService.raiseAlert());
+    }
+}
